6e1f5d9762444d6a753abe7646b80c81e0aa96c7
Štěpán Zapadlo <stepan.zapadlo@gmail.com>
2025-10-16 17:17:49 +0200

    Add CI/CD pipeline to detect new versions of amml-utils Add shell.nix for ease of development/testing for @sceptri

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,6 @@
+---
+title: New version of amml-utils available!
+assignees: sceptri
+labels: enhancement
+---
+New version of `amml-utils` was found!
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{}: let
+  pkgs = import <nixpkgs> {};
+  unstablePkgs = import <nixos-unstable> {};
+
+  lib = pkgs.lib;
+  stdenv = pkgs.stdenv;
+
+  config = import <config> {};
+  configPath = builtins.getEnv "NIXOS_CONFIGURATION_DIR" + "/.";
+  currentDir = ./.;
+in
+  pkgs.mkShell {
+    buildInputs = [
+      pkgs.act
+    ];
+
+    shellHook = ''
+      echo "Welcome to the AMML Docker base image shell"
+      export PS1="\\[\\033[1;36m\\][\\u@AMML Python Base(\\h):\\w]$\\[\\033[0m\\] "
+      export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+    '';
+  }
